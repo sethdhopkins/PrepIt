d4a47a1c07e92192fb7fca0f22405c9ad4883633
sethdhopkins <sethdhopkins2020@gmail.com>
2026-08-10 09:31:04 -0500

    Added models and test data for meals DB

---
+++ Source/Models/InventoryItem.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json;
+
+namespace Source.Models
+{
+    public class InventoryItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public string? Unit { get; set; }
+        public string? DateAdded { get; set; }
+        public required string Name { get; set; }
+        public required int InventoryId { get; set; }
+        public int ApiId { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this, this.GetType());
+        }
+    }
+}
---
+++ Source/Models/SavedRecipe.cs
@@ -0,0 +1,12 @@
+﻿namespace Source.Models
+{
+    public class SavedRecipe
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Image { get; set; }
+        public required string Instructions { get; set; }
+        public required string Ingredients { get; set; }
+        public required int UserId { get; set; }
+    }
+}
