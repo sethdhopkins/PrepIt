fbcd4a8bccfb391eb17b383b126766f540eddf7a
sethdhopkins <sethdhopkins2020@gmail.com>
2026-08-16 14:00:37 -0500

    Initital commit

--- Source/obj/Debug/net10.0/Source.AssemblyInfo.cs
+++ Source/obj/Debug/net10.0/Source.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Source")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c7c3226e4ff501c4ad7c54fad05d7b43503c368")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd0e239a88555fd3611a70a703d8be8f446b2171")]
 [assembly: System.Reflection.AssemblyProductAttribute("Source")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Source")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
