30ef62440bd797610778e301550ca43f673a40e9
sethdhopkins <sethdhopkins2020@gmail.com>
2026-08-18 14:05:32 -0500

    Demo Deploy setup

--- Source/obj/Debug/net10.0/Source.AssemblyInfo.cs
+++ Source/obj/Debug/net10.0/Source.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Source")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd0e239a88555fd3611a70a703d8be8f446b2171")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbcd4a8bccfb391eb17b383b126766f540eddf7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Source")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Source")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
