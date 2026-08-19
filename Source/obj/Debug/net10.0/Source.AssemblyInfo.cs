1f53aefbe8db053f041f5ab5073b301388ba5ef7
sethdhopkins <sethdhopkins2020@gmail.com>
2026-08-19 13:56:22 -0500

    Update

--- Source/obj/Debug/net10.0/Source.AssemblyInfo.cs
+++ Source/obj/Debug/net10.0/Source.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Source")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+638006fd459e80d51f1729d14ff1b89d99cee734")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5018546e011b917d71e08c9704856d6e278b0d06")]
 [assembly: System.Reflection.AssemblyProductAttribute("Source")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Source")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
