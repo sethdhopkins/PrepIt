6fd15bf71c2a35ea3d2319746769bf0b24fdab70
sethdhopkins <sethdhopkins2020@gmail.com>
2026-08-19 13:50:15 -0500

    Switched branches

--- Source/obj/Debug/net10.0/Source.AssemblyInfo.cs
+++ Source/obj/Debug/net10.0/Source.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Source")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eba65b457f4fee6a267e427ce44846990fb6d372")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c660fdfd2939fbcff46eb63e43f264ac50d3f75b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Source")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Source")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
